bec873b6eac148bf61cf29ed35166f017933b8d9
emirdeliz <emir.marques@condoconta.com>
2022-09-07 21:40:04 -0300

    feat: Work in progress

--- make-symlink.sh
+++ make-symlink.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # It's used to make the workspace config visible in the vscode.
 
-ln -sf node-base-docker/.devcontainer .devcontainer
+ln -sf emirdeliz-vs-workspace-container/.devcontainer .devcontainer
